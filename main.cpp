db367eebd2cb62237f7976edfaec380bdcff305b
PatrickCharriere <patrick.charriere.pro@gmail.com>
2015-02-01 14:49:15 +0100

    Premier test parseur csv fonctionnel

--- main.cpp
+++ main.cpp
@@ -1,27 +1,87 @@
 #include "main.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <istream>
+using std::cout;
+using std::endl;
+
+std::vector<std::string> csv_read_row(std::istream &in, char delimiter);
+std::vector<std::string> csv_read_row(std::string &in, char delimiter);
 
 int main(int argc, char* argv[]) {
-	point_t centre;
-	centre.x = 5;
-	centre.y = 5;
 
-	object_t robot = robot_main;
-	object_t yellowStand = item_stand_yellow;
+    std::ifstream in("map.csv");
+    if (in.fail()) return (cout << "File not found" << endl) && 0;
+    while(in.good())
+    {
+        std::vector<std::string> row = csv_read_row(in, ';');
+        for(int i=0, leng=row.size(); i<leng; i++)
+            cout << "[" << row[i] << "]" << "\t";
+        cout << endl;
+    }
+    in.close();
 
-	Rectangle* mainRobotShape = new Rectangle(centre, robot, 10, 10, 0);
-	Circle* yellowStand1 = new Circle(centre, yellowStand, 5);
+    std::string line;
+    in.open("input.csv");
+    while(getline(in, line)  && in.good())
+    {
+        std::vector<std::string> row = csv_read_row(line, ';');
+        for(int i=0, leng=row.size(); i<leng; i++)
+            cout << "[" << row[i] << "]" << "\t";
+        cout << endl;
+    }
+    in.close();
 
-	Mesh* rectangleMesh = new Mesh(mainRobotShape);
-	Mesh* circleMesh = new Mesh(yellowStand1);
 
-	//circleMesh->TerminalPrintMesh();
-
-	/*for(int i=0; i<=360; i+=1) {
-		mainRobotShape->SetAngle(i);
-		rectangleMesh->GenerateFromShape(mainRobotShape);
-		rectangleMesh->TerminalPrintMesh();
-		usleep(10000);
-		system("clear");
-	}*/
 	return 0;
+}
+
+std::vector<std::string> csv_read_row(std::string &line, char delimiter)
+{
+    std::stringstream ss(line);
+    return csv_read_row(ss, delimiter);
+}
+
+std::vector<std::string> csv_read_row(std::istream &in, char delimiter)
+{
+    std::stringstream ss;
+    bool inquotes = false;
+    std::vector<std::string> row;//relying on RVO
+    while(in.good())
+    {
+        char c = in.get();
+        if (!inquotes && c=='"') //beginquotechar
+        {
+            inquotes=true;
+        }
+        else if (inquotes && c=='"') //quotechar
+        {
+            if ( in.peek() == '"')//2 consecutive quotes resolve to 1
+            {
+                ss << (char)in.get();
+            }
+            else //endquotechar
+            {
+                inquotes=false;
+            }
+        }
+        else if (!inquotes && c==delimiter) //end of field
+        {
+            row.push_back( ss.str() );
+            ss.str("");
+        }
+        else if (!inquotes && (c=='\r' || c=='\n') )
+        {
+            if(in.peek()=='\n') { in.get(); }
+            row.push_back( ss.str() );
+            return row;
+        }
+        else
+        {
+            ss << c;
+        }
+    }
 }
